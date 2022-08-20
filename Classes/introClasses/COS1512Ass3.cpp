b325521a8385ce03e25713fdacb558eee38ad251
Ash <77675657+ThePowerfulProgrammer@users.noreply.github.com>
2022-08-21 01:09:43 +0200

    fixing minor bugs on Q1

--- Classes/introClasses/COS1512Ass3.cpp
+++ Classes/introClasses/COS1512Ass3.cpp
@@ -1,3 +1,4 @@
+// Use a class in a program to print an address
 #include <iostream>
 #include <string>
 
@@ -6,12 +7,45 @@ using namespace std;
 class Address {
     public:
         Address(); // default constructor
+        void addstreetName(); // planned on 1 mutator but pdf states mutators
+        void addstreetNo();
+        void addcity();
+        void addPostalCode();
+        void displayAddress() const; // display address information
     private:
         string streetName;
         int streetNr;
         string city;
         string postalCode;
 };
+
+void Address::addstreetName() {
+    cout << "Street name:      " ;
+    getline(cin, streetName, '\n');
+}
+
+void Address::addstreetNo() {
+    cout << "Street number:    " ;
+    cin >> streetNr ;
+}
+
+void Address::addcity() {
+    cout << "City:             " ;
+    cin >> city;
+}
+
+void Address::addPostalCode() {
+    cout << "Postal code:      " ;
+    cin >> postalCode ;
+}
+
+// What is proper address format? -> I will use -> streetNumber streetName, City, postalCode
+void Address::displayAddress() const {
+    cout << streetNr ;
+    cout << streetName;
+    cout << city;
+    cout << postalCode;
+}
 
 Address::Address() {
     // Values will be initialised whenever an Address is created
@@ -21,8 +55,11 @@ Address::Address() {
     postalCode = "0000";
 }
 
+
 int main() {
     Address address1;
-    cout << address1.streetName <<< endl;
+    address1.addstreetName();
+
+    address1.displayAddress();
     return 0;
 }
