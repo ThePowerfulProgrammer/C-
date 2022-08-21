fe83ed3878e0f10891f766e6258681db9363a003
Ash <77675657+ThePowerfulProgrammer@users.noreply.github.com>
2022-08-21 10:33:43 +0200

    completed Q2

--- Classes/introClasses/Q2.cpp
+++ Classes/introClasses/Q2.cpp
@@ -7,22 +7,72 @@ using namespace std;
 class ShowTicket {
     public:
         ShowTicket(); // default constructor
-        ShowTicket(string row,int seatNumber, bool ticketSold) {
+        ShowTicket(string row,int seatNumber) {
             ticketSold = false;
         }
-        bool isticketSold(); // checks if ticket sold
+        bool isTicketSold(); // checks if ticket sold
         void soldTicket(); // sets ticketSold = true;
         void displayTicket(); // displays all ticket data
+        
+        ~ShowTicket() {
+            cout << "Ticket used and destroyed" << endl;
+        }
     private:
-        string row;
+        int row;
         int seatNumber;
         bool ticketSold;
-       
 };
 
 
+ShowTicket::ShowTicket() {
+    // Values will be initialised whenever an Address is created
+    row = 0;
+    seatNumber = 0;
+    ticketSold = false;
+}
+
+bool ShowTicket::isTicketSold(){
+    if (ticketSold) {
+        return true;
+    }
+    return false;
+}
+
+void ShowTicket::soldTicket() {
+    ticketSold = true;
+}
+
+void ShowTicket::displayTicket() {
+    cout << "row " << row << endl;
+    cout << "seatNumber " << seatNumber << endl;
+    cout << "Ticket status " ;
+    if (ticketSold) {
+        cout << "sold" << endl;
+    } else {
+        cout << "not sold " << endl;
+    }
+    cout << endl << endl;
+}
 
 int main() {
- 
+    ShowTicket morbius;
+    ShowTicket TheBatman;
+    ShowTicket wonderWoman;
+    
+    morbius.isTicketSold();
+    morbius.soldTicket();
+    morbius.displayTicket();
+    
+    TheBatman.isTicketSold();
+    TheBatman.soldTicket();
+    TheBatman.displayTicket();
+    
+    wonderWoman.isTicketSold();
+    wonderWoman.soldTicket();
+    wonderWoman.displayTicket();
+    
+
+    cout << endl;
+
     return 0;
 }
