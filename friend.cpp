eedce811b41fe700459ccb35929d2708e9f0162b
Ash <77675657+ThePowerfulProgrammer@users.noreply.github.com>
2022-09-12 18:22:48 +0200

    Added friend function and Overloaded +

--- friend.cpp
+++ friend.cpp
@@ -9,10 +9,18 @@ class Money{
         int rands;
         int cents;
         Money() {rands = 10; cents =10;}
+        Money operator + (Money rightPosistion) {
+            Money temp;
+            temp.rands = rands + rightPosistion.rands;
+            temp.cents = cents + rightPosistion.cents;
+            return temp;
+        }
     private:
         friend void addMoney(Money & money);
 };
 
+
+// friend function 
 void addMoney(Money & money) {
     money.rands += 100;
     money.cents += 1000;
@@ -21,8 +29,13 @@ void addMoney(Money & money) {
 int main()
 {
     Money money1;
-    cout << endl;
+    Money money2;
+    
     addMoney(money1);
     cout << money1.rands << "  " << money1.cents << " " << endl;
+
+    Money money3 = money2 + money2;
+    cout << money3.rands << "  " << money3.cents << endl;
+    
     return 0;
 }
