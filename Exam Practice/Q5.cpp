a3e2ba43e817adf23b386a285af8187dc4c3d34d
Ash <77675657+ThePowerfulProgrammer@users.noreply.github.com>
2022-10-07 16:29:40 +0200

    added Q5

---
+++ Exam Practice/Q5.cpp
@@ -0,0 +1,20 @@
+
+
+#include <iostream>
+
+using namespace std;
+
+bool calcOutcome(float moduleMark);
+
+float calcAverage(float assignMarks[], int arraySize);
+
+void multiply(int & x);
+
+
+int main()
+{
+
+
+
+    return 0;
+}
